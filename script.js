ceff31f225d2206ee112b2d7bc6a843f5d2e3f42
Agustin Zanotta <tintin9409@hotmail.com>
2023-08-03 15:48:36 +1200

    toggle en la clase que muestra el desktopMenu en pantalla,agrege un pinter en el email.

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-const navEmail ;
+const navEmail = document.querySelector('.navbar-email');
+const desktopmenu = document.querySelector('.desktop-menu');
+
+navEmail.addEventListener('click', toggledesktopmenu);
+console.log("js funcionando");
+function toggledesktopmenu() {
+    desktopmenu.classList.toggle ("inactive");
+    console.log("function funcionando");
+
+}
